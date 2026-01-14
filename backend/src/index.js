1c84ba8510d4e283a629d5276d5d96e65b02f32e
TG0T <victor.mendoza.cl@gmail.com>
2026-01-14 04:08:39 -0300

    solucion urls

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,21 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const client = redis.createClient({
-    url: process.env.REDIS_URL || 'redis://34.68.91.195:6379'
+    url: process.env.REDIS_URL || 'redis://db:6379'
 });
 
 client.on('error',(err) => console.log('Redis Client Error', err));
 client.connect().then(() => console.log('Conectado a Redis'));
 
+app.get("/", (req, res)=>{
+    res.send("backend funcionando")
+});
+
 app.post('/acortador', async (req, res)=>{
     const { longUrl } = req.body;
-
-    // --- DEBUG ---
-    console.log('Tipo de dato:', typeof longUrl);
-    console.log('Valor:', longUrl);
-    // -------------
     const shortId = nanoid(6);
-
     await client.set(shortId, longUrl);
     res.json({shortUrl: `http://34.68.91.195:3000/${shortId}`});
 });
