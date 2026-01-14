546efd5a5626e0f0263935afd69ed1fd8e8ca004
TG0T <victor.mendoza.cl@gmail.com>
2026-01-14 02:09:09 -0300

    Init + vps google cloud

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const redis = require('redis');
+const { nanoid } = require('nanoid');
+
+const app = express();
+app.use(express.json());
+
+const client = redis.createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
+
+client.on('error',(err) => console.log('Redis Client Error', err));
+client.connect().then(() => console.log('Conectado a Redis'));
+
+app.post('/acortador', async (req, res)=>{
+    const { longUrl } = req.body;
+    const shortId = nanid(6);
+    await client.set({ shortUrl: `http://localhost:3000/${shortId}` });
+});
+
+app.get('/id', async (req, res) =>{
+    const { id } = req.params;
+    const longUrl = await client.get(id);
+    if (longUrl) return res.redirect(longUrl);
+    res.status(404).send('URL no encontrada');
+});
+
+app.listen(3000, () => console.log('Servidor en puerto 3000'));
